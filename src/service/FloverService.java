eeea7d6b9aba78e950aaf85009ec2319ed1b9101
Dilbar <bdilbar430@gmail.com>
2022-12-22 08:17:33 +0500

    Initial commit

---
+++ src/service/FloverService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+public interface FloverServiceImpl <T>{
+    String addFlowers(List<T> t);
+//        List<T> getAllFlowers();
+//        List<T> getFlowersFinByColor(String color);
+//        Map<String, List<T>> groupingFlowersByColor();
+//        int getSumAllFlowers();
+//        List<T> getAllFlowersSortByPrice(String ascendingOrDescending);
+}
